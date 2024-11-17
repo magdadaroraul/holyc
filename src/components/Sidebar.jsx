90cce442c2e3a60298fb68bd157e9d85f27277fb
Raul Magdadaro Jr <magsasaka9599@gmail.com>
2024-11-18 01:54:24 +0800

    add product feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,9 @@ const Sidebar = () => {
     <div className="d-flex flex-column p-3 bg-light" style={{ width: '250px', height: '100vh' }}>
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
-          
+          <Link to="/dashboard/products" className="nav-link">
+            Products
+          </Link>
         </li>
       </ul>
     </div>
