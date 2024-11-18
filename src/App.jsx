1cceacae2ff8926a19b747e30860cfad18dc679c
Raul Magdadaro Jr <magsasaka9599@gmail.com>
2024-11-18 17:40:15 +0800

    changes on registration

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
-import Dashboard from './components/Dashboard';
-import Users from './components/Users';
-import Products from './components/Products';
-import Profile from './components/Profile';
 
 function AppFunction() {
   return (
@@ -14,11 +10,6 @@ function AppFunction() {
         <Route path= "/" element={<Navigate to="/login"/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="profile" element={<Profile />} /> 
-          <Route path="users" element={<Users />} />
-          <Route path="products" element={<Products />} />
-        </Route>
       </Routes>
     </Router>
   );
