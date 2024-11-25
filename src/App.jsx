71a40d9323818fbeae992e09f60bfa9a44175fc9
Raul Magdadaro Jr <magsasaka9599@gmail.com>
2024-11-25 14:14:18 +0800

    changes on login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './components/Dashboard';
+import Products from './components/Products';
+import Profile from './components/Profile';
+import Logout from './pages/Logout';
+import Favorites from './components/Favorites';
+import Mine from './components/Mine';
+import Yellowbucket from './components/Yellowbucket';
+import Checkout from './components/Checkout';
 
 
 function AppFunction() {
